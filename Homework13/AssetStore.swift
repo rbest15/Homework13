6d8ccfb049972476033b523142b1546a63591c15
moremist <rbest15@rambler.ru>
2021-04-26 20:06:22 +0300

    Error fixed, composition finished, second transition problem

---
+++ Homework13/AssetStore.swift
@@ -0,0 +1,155 @@
+import Foundation
+import AVFoundation
+import UIKit
+
+class AssetStore {
+    let video1: AVAsset
+    let video2: AVAsset
+    let video3: AVAsset
+    let music1: AVAsset
+    let music2: AVAsset
+    
+    var delegate : ViewController?
+    var rect : CGRect?
+    
+    init(video1: AVAsset, video2: AVAsset, video3: AVAsset, music1: AVAsset, music2: AVAsset) {
+        self.video1 = video1
+        self.video2 = video2
+        self.video3 = video3
+        self.music1 = music1
+        self.music2 = music2
+    }
+    
+    static func asset(_ res: String, _ type: String) -> AVAsset {
+        guard let path = Bundle.main.path(forResource: res, ofType: type) else {
+            print("error")
+            fatalError()
+        }
+        let url = URL(fileURLWithPath: path)
+        return AVAsset(url: url)
+    }
+    
+    static func test() -> AssetStore {
+        return AssetStore(video1: asset("video1", "mp4"), video2: asset("video2", "mp4"), video3: asset("video3", "mp4"), music1: asset("music1", "mp3"), music2: asset("music2", "mp3"))
+    }
+
+    func compose() -> (AVAsset, AVVideoComposition) {
+        
+        //MARK: - create composition
+        let composition = AVMutableComposition()
+        guard let videoTrack1 = composition.addMutableTrack(withMediaType: .video, preferredTrackID: Int32(kCMPersistentTrackID_Invalid)) else { fatalError() }
+        guard let videoTrack2 = composition.addMutableTrack(withMediaType: .video, preferredTrackID: Int32(kCMPersistentTrackID_Invalid)) else { fatalError() }
+        guard let videoTrack3 = composition.addMutableTrack(withMediaType: .video, preferredTrackID: Int32(kCMPersistentTrackID_Invalid)) else { fatalError() }
+        guard let audioTrack1 = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: Int32(kCMPersistentTrackID_Invalid)) else { fatalError() }
+        guard let audioTrack2 = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: Int32(kCMPersistentTrackID_Invalid)) else { fatalError() }
+        
+        guard let bounds = self.rect else {
+            fatalError()
+        }
+        
+        let transitionDuration1 = CMTime(seconds: 1, preferredTimescale: 600)
+        let transitionDuration2 = CMTime(seconds: 1, preferredTimescale: 600)
+        let transitionRange1 = CMTimeRange(start: video1.duration - transitionDuration1, duration: transitionDuration1)
+        let transitionRange2 = CMTimeRange(start: video1.duration - transitionDuration1 + video2.duration - transitionDuration2, duration: transitionDuration2)
+        
+        //MARK: - insert video tracks
+        try? videoTrack1.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: video1.duration), of: video1.tracks(withMediaType: .video)[0], at: CMTime.zero)
+        try? videoTrack2.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: video2.duration), of: video2.tracks(withMediaType: .video)[0], at: video1.duration - transitionDuration1)
+        try? videoTrack3.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: video3.duration), of: video3.tracks(withMediaType: .video)[0], at: video1.duration  - transitionDuration1 + video2.duration - transitionDuration2)
+        
+        //MARK: - insert audio tracks
+        try? audioTrack1.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: video1.duration + video2.duration - transitionDuration1), of: music1.tracks(withMediaType: .audio)[0], at: CMTime.zero)
+        try? audioTrack2.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: video3.duration - transitionDuration2), of: music2.tracks(withMediaType: .audio)[0], at: video1.duration + video2.duration - transitionDuration1)
+        
+        //MARK: - create pass through 1
+        let passTroughInstruction1 = AVMutableVideoCompositionInstruction()
+        passTroughInstruction1.timeRange = CMTimeRange(start: CMTime.zero, duration: video1.duration - transitionDuration1)
+        
+        let passTroughLayerInstruction1 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack1)
+        passTroughLayerInstruction1.setTransform(videoTrack1.preferredTransform, at: CMTime.zero)
+        passTroughInstruction1.layerInstructions = [passTroughLayerInstruction1]
+        
+        //MARK: - reate pass through 2
+        let passTroughInstruction2 = AVMutableVideoCompositionInstruction()
+        passTroughInstruction2.timeRange = CMTimeRange(start: video1.duration, duration: video2.duration - transitionDuration1 - transitionDuration2)
+        
+        let passTroughLayerInstruction2 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack2)
+        passTroughLayerInstruction2.setTransform(videoTrack2.preferredTransform, at: CMTime.zero)
+        passTroughInstruction2.layerInstructions = [passTroughLayerInstruction2]
+        
+        //MARK: - create pass through 3
+        let passTroughInstruction3 = AVMutableVideoCompositionInstruction()
+        passTroughInstruction3.timeRange = CMTimeRange(start: video1.duration - transitionDuration1 + video2.duration, duration: video3.duration - transitionDuration2)
+        
+        let passTroughLayerInstruction3 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack3)
+        passTroughLayerInstruction3.setTransform(videoTrack3.preferredTransform, at: CMTime.zero)
+        passTroughInstruction3.layerInstructions = [passTroughLayerInstruction3]
+       
+        //MARK: - create instruction 1
+        let instruction1 = AVMutableVideoCompositionInstruction()
+        instruction1.timeRange = transitionRange1
+        let fadeOutLayerInstruction1 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack1)
+        let fadeInLayerInstruction1 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack2)
+        
+        //MARK: - create instruction 2
+        let instruction2 = AVMutableVideoCompositionInstruction()
+        instruction2.timeRange = transitionRange2
+        let fadeOutLayerInstruction2 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack2)
+        let fadeInLayerInstruction2 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack3)
+        
+        //MARK: - fade out/ fade in animation settings
+        fadeOutLayerInstruction1.setTransform(video1.preferredTransform, at: CMTime.zero)
+        fadeInLayerInstruction1.setTransform(video2.preferredTransform, at: CMTime.zero)
+        
+        fadeInLayerInstruction1.setTransformRamp(fromStart: CGAffineTransform(translationX: -1170, y: 1), toEnd: CGAffineTransform(translationX: 1, y: 1), timeRange: transitionRange1)
+        fadeOutLayerInstruction1.setTransformRamp(fromStart: CGAffineTransform(translationX: 1, y: 1), toEnd: CGAffineTransform(translationX: 1170, y: 1), timeRange: transitionRange1)
+        
+    
+        fadeOutLayerInstruction2.setTransform(video2.preferredTransform, at: CMTime.zero)
+        fadeInLayerInstruction2.setTransform(video3.preferredTransform, at: CMTime.zero)
+        fadeInLayerInstruction2.setTransform(CGAffineTransform(translationX: 1.5, y: 1.5), at: transitionRange2.start)
+    
+        fadeInLayerInstruction2.setTransformRamp(fromStart: CGAffineTransform(scaleX: 0.001, y: 0.001), toEnd: CGAffineTransform(scaleX: 1, y: 1), timeRange: transitionRange2)
+        
+        //MARK: - composition compilating
+        let videoComposition = AVMutableVideoComposition(propertiesOf: composition)
+        
+        instruction1.layerInstructions = [fadeInLayerInstruction1, fadeOutLayerInstruction1]
+        instruction2.layerInstructions = [fadeInLayerInstruction2, fadeOutLayerInstruction2]
+        videoComposition.instructions = [passTroughInstruction1, instruction1, passTroughInstruction2, instruction2, passTroughInstruction3]
+        
+//        print(composition.duration.seconds.rounded(), videoComposition.instructions.forEach({ (comp) in
+//            print(comp.timeRange.duration.seconds.rounded())
+//        }))
+        return (composition, videoComposition)
+    }
+    
+    func export(asset: AVAsset, completiton: @escaping (AVAssetExportSession) -> Void) {
+        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
+            fatalError()
+        }
+        var fileName = ""
+        
+        let alert = UIAlertController(title: "Name", message: "Provide a name for file:", preferredStyle: .alert)
+        alert.addTextField { (textfield) in
+            return
+        }
+        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
+            self.delegate?.dismiss(animated: true, completion: nil)
+            fileName = alert.textFields![0].text ?? "defaultName"
+            let url = documentsDirectory.appendingPathComponent(fileName)
+            guard let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPreset1280x720) else {
+                fatalError()
+            }
+            exporter.outputURL = url
+            exporter.outputFileType = .mp4
+            exporter.exportAsynchronously {
+                DispatchQueue.main.async {
+                    completiton(exporter)
+                }
+            }
+        }))
+        self.delegate?.present(alert, animated: true)
+    }
+}
+
--- Homework13/ViewController.swift
+++ Homework13/ViewController.swift
@@ -7,9 +7,8 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        store.vc = self
+        store.rect = self.view.frame
         createSaveButton()
-        
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
@@ -28,8 +27,9 @@ class ViewController: UIViewController {
         let player = AVPlayer(playerItem: playerItem)
         let playerLayer = AVPlayerLayer(player: player)
         playerLayer.frame = self.view.bounds
+//        playerLayer.anchorPoint = self.view.center
         self.view.layer.addSublayer(playerLayer)
-        player.play()
+        player.playImmediately(atRate: 2)
     }
     
     func createSaveButton() {
@@ -65,120 +65,3 @@ class ViewController: UIViewController {
 
 }
 
-class AssetStore {
-    let video1: AVAsset
-    let video2: AVAsset
-    let video3: AVAsset
-    let music1: AVAsset
-    let music2: AVAsset
-    
-    var vc : ViewController?
-    
-    init(video1: AVAsset, video2: AVAsset, video3: AVAsset, music1: AVAsset, music2: AVAsset) {
-        self.video1 = video1
-        self.video2 = video2
-        self.video3 = video3
-        self.music1 = music1
-        self.music2 = music2
-    }
-    
-    static func asset(_ res: String, _ type: String) -> AVAsset {
-        guard let path = Bundle.main.path(forResource: res, ofType: type) else {
-            print("error")
-            fatalError()
-        }
-        let url = URL(fileURLWithPath: path)
-        return AVAsset(url: url)
-    }
-    
-    static func test() -> AssetStore {
-        return AssetStore(video1: asset("video1", "mp4"), video2: asset("video2", "mp4"), video3: asset("video3", "mp4"), music1: asset("music1", "mp3"), music2: asset("music2", "mp3"))
-    }
-    
-
-    
-    func compose() -> (AVAsset, AVVideoComposition) {
-        let composition = AVMutableComposition()
-        guard let videoTrack1 = composition.addMutableTrack(withMediaType: .video, preferredTrackID: Int32(kCMPersistentTrackID_Invalid)) else { fatalError() }
-        guard let videoTrack2 = composition.addMutableTrack(withMediaType: .video, preferredTrackID: Int32(kCMPersistentTrackID_Invalid)) else { fatalError() }
-        guard let audioTrack = composition.addMutableTrack(withMediaType: .audio, preferredTrackID: Int32(kCMPersistentTrackID_Invalid)) else { fatalError() }
-        
-        let transition1Duration = CMTime(seconds: 1, preferredTimescale: 600)
-        
-        try? videoTrack1.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: video1.duration), of: video1.tracks(withMediaType: .video)[0], at: CMTime.zero)
-        try? videoTrack2.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: video2.duration), of: video2.tracks(withMediaType: .video)[0], at: video1.duration - transition1Duration)
-        try? audioTrack.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: video1.duration + video2.duration - transition1Duration), of: music1.tracks(withMediaType: .audio)[0], at: CMTime.zero)
-        
-        let passTroughInstruction1 = AVMutableVideoCompositionInstruction()
-        passTroughInstruction1.timeRange = CMTimeRange(start: CMTime.zero, duration: video1.duration - transition1Duration)
-        
-        let passTroughLayerInstruction1 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack1)
-        passTroughLayerInstruction1.setTransform(videoTrack1.preferredTransform, at: CMTime.zero)
-        passTroughInstruction1.layerInstructions = [passTroughLayerInstruction1]
-        
-        let passTroughInstruction2 = AVMutableVideoCompositionInstruction()
-        passTroughInstruction2.timeRange = CMTimeRange(start: video1.duration, duration: video2.duration - transition1Duration)
-        
-        let passTroughLayerInstruction2 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack2)
-        passTroughLayerInstruction2.setTransform(videoTrack2.preferredTransform, at: CMTime.zero)
-        passTroughInstruction2.layerInstructions = [passTroughLayerInstruction2]
-       
-        let transition1Range = CMTimeRange(start: video1.duration - transition1Duration, end: transition1Duration)
-        
-        let videoComposition = AVMutableVideoComposition(propertiesOf: composition)
-        let instruction1 = AVMutableVideoCompositionInstruction()
-        instruction1.timeRange = transition1Range
-        let fadeOutLayerInstruction1 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack1)
-        let fadeInLayerInstruction1 = AVMutableVideoCompositionLayerInstruction(assetTrack: videoTrack2)
-        
-        fadeOutLayerInstruction1.setTransform(video1.preferredTransform, at: CMTime.zero)
-        fadeOutLayerInstruction1.setOpacityRamp(fromStartOpacity: 1, toEndOpacity: 0, timeRange: transition1Range)
-        fadeInLayerInstruction1.setTransform(video2.preferredTransform, at: CMTime.zero)
-        fadeInLayerInstruction1.setOpacityRamp(fromStartOpacity: 0, toEndOpacity: 1, timeRange: transition1Range)
-        
-        instruction1.layerInstructions = [fadeOutLayerInstruction1, fadeInLayerInstruction1]
-        videoComposition.instructions = [passTroughInstruction1, instruction1, passTroughInstruction2]
-        
-//        func insertVideo(asset: AVAsset, at: CMTime) {
-//            try? videoTrack1.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: asset.duration), of: asset.tracks(withMediaType: .video)[0], at: at)
-//        }
-//        func insertAudio(asset: AVAsset, duration: CMTime ,at: CMTime) {
-//            try? audioTrack.insertTimeRange(CMTimeRange(start: CMTime.zero, duration: duration), of: asset.tracks(withMediaType: .audio)[0], at: at)
-//        }
-//        insertVideo(asset: video1, at: CMTime.zero)
-//        insertVideo(asset: video2, at: video1.duration)
-//        insertVideo(asset: video3, at: video1.duration + video2.duration)
-//        insertAudio(asset: music1, duration: video1.duration + video2.duration - transition1Duration ,at: CMTime.zero)
-//        insertAudio(asset: music2, duration: CMTime(seconds: 15, preferredTimescale: .max) ,at: CMTime(seconds: 30, preferredTimescale: .max))
-
-        return (composition, videoComposition)
-    }
-    
-    func export(asset: AVAsset, completiton: @escaping (AVAssetExportSession) -> Void) {
-        guard let documentsDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
-            fatalError()
-        }
-        var fileName = ""
-        
-        let alert = UIAlertController(title: "Name", message: "Provide a name for file:", preferredStyle: .alert)
-        alert.addTextField { (textfield) in
-            return
-        }
-        alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (action) in
-            self.vc?.dismiss(animated: true, completion: nil)
-            fileName = alert.textFields![0].text ?? "defaultName"
-            let url = documentsDirectory.appendingPathComponent(fileName)
-            guard let exporter = AVAssetExportSession(asset: asset, presetName: AVAssetExportPreset1280x720) else {
-                fatalError()
-            }
-            exporter.outputURL = url
-            exporter.outputFileType = .mp4
-            exporter.exportAsynchronously {
-                DispatchQueue.main.async {
-                    completiton(exporter)
-                }
-            }
-        }))
-        self.vc?.present(alert, animated: true)
-    }
-}
